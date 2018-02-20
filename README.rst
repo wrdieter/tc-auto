33148f836821cfbfaa422e0e2460cd7ae07e2c64
William R. Dieter <dieter@acm.org>
2018-02-19 20:56:58 -0800

    Assume the biggest file is the main title, rather than the first
    In several cases, the first has not been the actual movie.  Using the
    biggest does not work.  For example, if the disc contains a collection,
    of movies or if there is a full length director's cut that is longer.
    
    Signed-off-by: William R. Dieter <dieter@acm.org>

--- README.rst
+++ README.rst
@@ -1,19 +1,9 @@
 TODO Notes:
 
-* Currently the first tile on a movie DVD is assumed to be the main
-  feature and the rest are assumed to be supporting content.  They
-  are all dumped into a "Behind The Scenes" directory for later
-  sorting.  That is a place Plex should put them and make them
-  visible.  There are currently two problems:
-
-  1. extra content is not showing up.  I think this is because the
-     file name is just the, filename from makemkv, rather than the
-     movie title.
-
-  2. The first title is not always the main feature.  Maybe picking
-     the biggest would be a better heuristic.
+* Surely, there must be a way to get the titles from the DVD menu
 
   Long term, it would be nice to have a database mapping makemkv titles
-  to content names and categories (Behind the Scenes, Trailer, etc.)
+  to content names and categories (Behind the Scenes, Trailer, etc.) if
+  there is not a way to read it from the disc directly.
 
-* Need a license
+* Need a license and license file
--- transcode.py
+++ transcode.py
@@ -4,30 +4,54 @@
 import argparse
 import glob
 import os
+import shutil
 import subprocess
 import yaml
 
 def main():
     parser = argparse.ArgumentParser(description="Batch transcode MKV files into mp4's")
+    parser.add_argument('--input-dir', default='incoming',
+                        help='Directory containing mkv files to be transcoded')
+    parser.add_argument('--output-dir', default='Movies',
+                        help='Directory in which to write transcoded files')
+    parser.add_argument('--archive-dir', default='loaded',
+                        help='Directory in which to place source of '
+                             'sucessfully transcoded files')
     args = parser.parse_args()
 
-    incoming = 'incoming'
-    for movie_title in os.listdir(incoming):
+    transcode_tmp = '/tmp/transcode'
+    for movie_title in os.listdir(args.input_dir):
         print(movie_title)
-        for mkv in glob.glob(os.path.join(incoming, movie_title, '*.mkv')):
-            print(mkv)
+        titles = glob.glob(os.path.join(args.input_dir, movie_title, '*.mkv'))
+        if len(titles) == 0:
+            continue
+        titles = sorted(titles, key=os.path.getsize, reverse=True)
+        # ASSUME: biggest .mkv file is the main feature, and others are extras
+        main_mkv = titles.pop(0)
+        dest_dir = os.path.join(transcode_tmp, movie_title)
+        dest_file = os.path.join(dest_dir, movie_title + '.mp4')
+        if not os.path.isdir(dest_dir):
+            os.makedirs(dest_dir)
+        transcode(main_mkv, dest_file)
+
+        # Put all the extras into "Behind The Scenes" and sort them out later
+        dest_dir = os.path.join(dest_dir, "Behind The Scenes")
+        if not os.path.isdir(dest_dir):
+            os.makedirs(dest_dir)
+        for mkv in titles:
             _, mkv_file = os.path.split(mkv)
-            dest_dir = os.path.join('transcoded', movie_title)
-            if mkv_file != 'title00.mkv':
-                dest_dir = os.path.join(dest_dir, "Behind The Scenes")
-                dest_file = os.path.join(dest_dir, mkv_file.replace('.mkv','.mp4'))
-            else:
-                dest_file = os.path.join(dest_dir, movie_title + '.mp4')
-            if not os.path.isdir(dest_dir):
-                os.makedirs(dest_dir)
-            #print(['HandBrakeCLI', '-Z', 'Super HQ 1080p30 Surround', '-O',
-            subprocess.run(['HandBrakeCLI', '-Z', 'Super HQ 1080p30 Surround', '-O',
-                           '-i', mkv, '-o', dest_file])
+            dest_file = os.path.join(dest_dir, mkv_file.replace('.mkv','.mp4'))
+            transcode(mkv, dest_file)
+        # Move completed movies to the done area and archive input
+        shutil.move(os.path.join(transcode_tmp, movie_title), args.output_dir)
+        shutil.move(os.path.join(args.input_dir, movie_title), args.archive_dir)
+
+
+def transcode(in_path, out_path):
+    """Transcode the movie"""
+    #print(['HandBrakeCLI', '-Z', 'Super HQ 1080p30 Surround', '-O',
+    subprocess.run(['HandBrakeCLI', '-Z', 'Super HQ 1080p30 Surround', '-O',
+                   '-i', in_path, '-o', out_path])
 
 
 if __name__ == "__main__":
